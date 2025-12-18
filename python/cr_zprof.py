4192a2b5e1b0d94ce65a5c21de16498345f34af1
Chang-Goo Kim <cgkim@astro.princeton.edu>
2025-12-18 13:01:58 -0500

    Removed some unused imports

--- python/cr_zprof.py
+++ python/cr_zprof.py
@@ -2,11 +2,8 @@ import os
 import os.path as osp
 import glob
 
-from python import pdf
 import xarray as xr
-
 import numpy as np
-import matplotlib.pyplot as plt
 
 from scipy.optimize import curve_fit
 
